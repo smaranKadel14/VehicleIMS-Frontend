84972f074f0fcba461a09798b9ffa364fe3c5b89
Smaran Kadel <kadelsmaran45@gmail.com>
2026-04-28 01:22:41 +0545

    Fixed routes for auth pages

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "../pages/admin/Login";
-import Register from "../pages/admin/Register";
-import ForgotPassword from "../pages/admin/ForgotPassword";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import ForgotPassword from "../pages/auth/ForgotPassword";
 import VendorManagement from "../pages/VendorManagement";
 import CustomerDirectory from "../pages/CustomerDirectory";
 
